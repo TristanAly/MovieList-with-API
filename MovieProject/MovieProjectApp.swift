e033ac50d9addde845682d26df6d6644deb105f3
Tristan Aly <tristan.aly@gmail.com>
2022-10-03 13:31:48 +0200

    Initial Commit

---
+++ MovieProject/MovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieProjectApp.swift
+//  MovieProject
+//
+//  Created by apprenant1 on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
